0d9d75fe135d935bf8f07440198129999f88e486
minhthuanus <minhthuanus@gmail.com>
2015-03-27 20:36:36 +0700

    Nop bai tap

--- NguyenVanThuan/For D.py
+++
@@ -1,9 +0,0 @@
-file=open("D:\\word.txt","r+")
-wordcount={}
-for word in file.read().split():
-    if word not in wordcount:
-        wordcount[word] = 1
-    else:
-        wordcount[word] += 1
-print (word,wordcount)
-file.close();
